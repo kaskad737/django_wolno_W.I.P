163582bab03b57b7c96d7fa05ee4bfea90c95640
apti <Archakov.Apti@email.cz>
2023-11-29 21:43:19 +0100

    finish 06 lesson

--- wolno/heatmap/views.py
+++ wolno/heatmap/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request): # HttpRequest
     return HttpResponse("Heatmap page")
+
+def info(request, test_id): # HttpRequest
+    return HttpResponse(f"Heatmap page - {test_id}")
