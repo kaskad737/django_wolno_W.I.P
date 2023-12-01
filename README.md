f0205663df5923fc98455ef3f316d32cc194546b
apti <Archakov.Apti@email.cz>
2023-12-01 10:52:40 +0100

    change readme. Create templates and adjust views to use templates

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # django_wolno
 
+The main goal of this project is to visualize the data that we receive through the website (you can see more about the data we receive and how we receive it in this project Link - [wolno-czu-project](https://github.com/kaskad737/wolno-czu-project).
+
+## Useful links
+
 Heatmap for wolno web
 <https://www.freecodecamp.org/news/interactive-heatmap-in-javascript/>
--- wolno/heatmap/views.py
+++ wolno/heatmap/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template.loader import render_to_string
 
-def index(request): # HttpRequest
-    return HttpResponse("Heatmap page")
 
-def info(request, test_id): # HttpRequest
+def index(request):  # HttpRequest
+    # t = render_to_string()
+    # return HttpResponse(t)
+    data = {
+        'title': 'Main page with heatmap',
+        'header': 'Main page header with heatmap'
+    }
+    return render(request, 'heatmap/index.html', context=data)
+
+
+def info(request, test_id):  # HttpRequest
     return HttpResponse(f"Heatmap page - {test_id}")
